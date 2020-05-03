3a78297159c6bb2b77f12baa202534a1f37d2832
Fizban <jackmckew2@gmail.com>
2020-05-03 22:35:58 +1000

    Multi working

--- playground-main.py
+++ playground-main.py
@@ -5,9 +5,9 @@ pandas_alive.output_file('test.mp4')
 
 df = pandas_alive.load_dataset()
 
-lc = df.diff().plot_animated(kind='line',write_to_file=False)
+lc = df.diff().plot_animated(kind='line',write_to_file=False,period_length=200)
 
-bc = df.plot_animated(kind='barh',write_to_file=False)
+bc = df.plot_animated(kind='barh',write_to_file=False,period_length=200)
 
 pandas_alive.animate_multiple_plots('Multi-test.gif',[bc,lc])
 
