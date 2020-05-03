88ad0a30577ee63d0b4bd373846435d6c9144e47
Fizban <jackmckew2@gmail.com>
2020-05-03 23:20:08 +1000

    Update README
    Former-commit-id: b3359fb827766a1917ee6df81ff4c02f8703d1d7

--- playground-main.py
+++ playground-main.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import pandas_alive
 
-pandas_alive.output_file('test.mp4')
-
 df = pandas_alive.load_dataset()
 
-lc = df.diff().plot_animated(kind='line',write_to_file=False,period_length=200)
+with open("example_dataset_table.md", "w") as md_file:
+    md_file.write(df.dropna().head(5).to_markdown())
 
-bc = df.plot_animated(kind='barh',write_to_file=False,period_length=200)
+pandas_alive.output_file('example-barh-chart.gif')
+df.plot_animated()
 
-pandas_alive.animate_multiple_plots('Multi-test.gif',[bc,lc])
+pandas_alive.output_file('example-barv-chart.gif')
+df.plot_animated(orientation='v')
+
+pandas_alive.output_file('example-line-chart.gif')
+df.diff().plot_animated(kind='line')
+
+animated_line_chart = df.diff().plot_animated(kind='line',write_to_file=False,period_length=200)
+
+animated_bar_chart = df.plot_animated(kind='barh',write_to_file=False,period_length=200)
+
+pandas_alive.animate_multiple_plots('example-bar-and-line-chart.gif',[animated_bar_chart,animated_line_chart])
 
 
