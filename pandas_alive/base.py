88a07e5fcb002779c11890e1ebed8f3bc1511d1f
Fizban <jackmckew2@gmail.com>
2020-05-05 07:12:52 +1000

    Move animated plot func to plot.py

--- pandas_alive/base.py
+++ pandas_alive/base.py
@@ -17,67 +17,7 @@ def load_dataset(name: str = "covid19") -> pd.DataFrame:
 
     return pd.read_csv(
         f"https://raw.githubusercontent.com/JackMcKew/pandas-alive/master/data/{name}.csv",
-        index_col="date",
-        parse_dates=["date"],
+        index_col=0,
+        parse_dates=[0],
     )
 
-
-def animate_multiple_plots(
-    filename: str,
-    plots: List[Union[BarChart, LineChart]],
-    title: str = None,
-    title_fontsize: Union[int, float] = 16,
-):
-    """ Plot multiple animated plots
-
-    Args:
-        plots (List[Union[_BarChartRace,_LineChartRace]]): List of plots to animate
-    """
-
-    # TODO Maybe add multichart class?
-
-    def update_all_graphs(frame):
-        for plot in plots:
-            try:
-                plot.anim_func(frame)
-            except:
-                raise UserWarning(
-                    f"{type(plot)} {plot.title} error plotting on frame {frame}, ensure all plots share index"
-                )
-
-    # Current just number of columns for number of plots
-    # TODO add option for number of rows/columns
-    fig, axes = plt.subplots(len(plots))
-
-    # Used for overlapping titles, supplot not considered so move down by 10%
-    # https://stackoverflow.com/questions/8248467/matplotlib-tight-layout-doesnt-take-into-account-figure-suptitle
-    fig.tight_layout(rect=[0, 0, 1, 0.9])
-
-    if title is not None:
-        fig.suptitle(title)
-
-    for num, plot in enumerate(plots):
-        axes[num].set_title(plot.title)
-        plot.ax = axes[num]
-
-        plot.init_func()
-
-    fps = 1000 / plots[0].period_length * plots[0].steps_per_period
-    interval = plots[0].period_length / plots[0].steps_per_period
-    anim = FuncAnimation(
-        fig,
-        update_all_graphs,
-        min([max(plot.get_frames()) for plot in plots]),
-        interval=interval,
-    )
-
-    extension = filename.split(".")[-1]
-    if extension == "gif":
-        anim.save(filename, fps=fps, writer="imagemagick")
-    else:
-        anim.save(filename, fps=fps)
-
-
-# def plot_animated_grid(children_plots: List):
-#     # TODO Implement multiple animated plots
-#     a = 0
