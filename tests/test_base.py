ea41f1574e7cca0bb7363b57802546986c0b333c
Fizban <jackmckew2@gmail.com>
2020-05-06 02:42:44 +1000

    Add tests
    Former-commit-id: ca5945834b86b7d0ed55754c2213aaff82c1b6e3

---
+++ tests/test_base.py
@@ -0,0 +1,11 @@
+import pytest
+
+import pandas_alive
+
+@pytest.fixture(scope="function")
+def covid_df():
+
+    # Load Iris Dataset:
+    covid_df = pandas_alive.load_dataset()
+
+    return covid_df
