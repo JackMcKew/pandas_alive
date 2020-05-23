adef67ba5c4bf7b164b37977d4d9a28cc48c3835
Fizban <jackmckew2@gmail.com>
2020-05-23 13:26:51 +1000

    Add pre commit hooks
    Former-commit-id: 8ccec500c50ef8cef5220a5c31140e2e9186abed

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -18,16 +18,52 @@ def covid_df():
     return covid_df
 
 
-def test_barh(covid_df):
+@pytest.mark.parametrize("orientation", ["h", "v"])
+@pytest.mark.parametrize("sort", ["desc", "asc"])
+@pytest.mark.parametrize("label_bars", [True, False])
+@pytest.mark.parametrize("bar_label_size", [6, 7, 8])
+@pytest.mark.parametrize("n_visible", [6, 7, 8])
+@pytest.mark.parametrize("fixed_order", [True, False])
+@pytest.mark.parametrize("perpendicular_bar_func", ["mean", "min", "median"])
+def test_bar_chart_race(
+    covid_df,
+    orientation,
+    sort,
+    label_bars,
+    bar_label_size,
+    n_visible,
+    fixed_order,
+    perpendicular_bar_func,
+):
 
-    animated_plot = covid_df.plot_animated(n_visible=5)
+    animated_plot = covid_df.plot_animated(
+        kind="race",
+        orientation=orientation,
+        sort=sort,
+        label_bars=label_bars,
+        bar_label_size=bar_label_size,
+        n_visible=n_visible,
+        fixed_order=fixed_order,
+        perpendicular_bar_func=perpendicular_bar_func,
+    )
 
 
-def test_barv(covid_df):
+# def test_barh(covid_df):
 
-    animated_plot = covid_df.plot_animated(orientation="v", n_visible=5)
+#     animated_plot = covid_df.plot_animated()
+
+#     assert True
+
+# @pytest.mark.parametrize("orientation",["h","v"])
+# def test_barv(covid_df,orientation):
+
+#     animated_plot = covid_df.plot_animated(orientation=orientation)
+
+#     assert True
 
 
-def test_line(covid_df):
+# def test_line(covid_df):
 
-    animated_plot = covid_df.diff().fillna(0).plot_animated(kind="line")
+#     animated_plot = covid_df.diff().fillna(0).plot_animated(kind="line")
+
+#     assert True
