ce53e88471f025f1125b6aaab9bbef7e562ff9df
Fizban <jackmckew2@gmail.com>
2020-06-01 12:13:11 +1000

    Update docs version & include interrogate badge

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Jack McKew"
 author = "Jack McKew"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.14h1"
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------
